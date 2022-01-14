62f15371b1cc12e2fe54622510718430ee6c61b7
Christian Albrecht <christian.albrecht@lorentz.de>
2022-01-14 18:19:16 +0100

    parse response header with nom

--- src/response_header.rs
+++ src/response_header.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use anyhow::{anyhow, Context, Error};
 
 use crate::bgx_response::ResponseCodes;
@@ -10,23 +8,25 @@ pub(crate) struct ResponseHeader {
     pub length: u16,
 }
 
-impl FromStr for ResponseHeader {
-    type Err = Error;
+impl TryFrom<&[u8]> for ResponseHeader {
+    type Error = Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         // typical header -> R000117\r\n but R and newline should already been removed by the parser
 
-        if s.len() != 6 {
+        if value.len() != 6 {
             return Err(anyhow!("Header code lenght is != 6"));
         }
 
+        let value = std::str::from_utf8(value)?;
+
         Ok(Self {
-            response_code: s
+            response_code: value
                 .get(..1)
                 .context("Couldn't get code number in header")?
                 .parse::<u8>()?
                 .into(),
-            length: s
+            length: value
                 .get(1..6)
                 .context("Couldn't get length numbers in header")?
                 .parse::<u16>()?,
@@ -36,9 +36,9 @@ impl FromStr for ResponseHeader {
 
 #[test]
 fn test_response_header_1() {
-    const HEADER: &str = "000009";
+    const HEADER: &[u8] = b"000009";
 
-    let h = ResponseHeader::from_str(HEADER).unwrap();
+    let h = ResponseHeader::try_from(HEADER).unwrap();
     let h2 = ResponseHeader {
         response_code: ResponseCodes::Success,
         length: 9,
@@ -50,9 +50,9 @@ fn test_response_header_1() {
 #[test]
 #[should_panic]
 fn test_response_header_2() {
-    const HEADER: &str = "000010";
+    const HEADER: &[u8] = b"000010";
 
-    let h = ResponseHeader::from_str(HEADER).unwrap();
+    let h = ResponseHeader::try_from(HEADER).unwrap();
     let h2 = ResponseHeader {
         response_code: ResponseCodes::Success,
         length: 9,
@@ -64,31 +64,31 @@ fn test_response_header_2() {
 #[test]
 #[should_panic]
 fn test_response_header_3() {
-    const HEADER: &str = "00009";
+    const HEADER: &[u8] = b"00009";
 
-    let _ = ResponseHeader::from_str(HEADER).unwrap();
+    let _ = ResponseHeader::try_from(HEADER).unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_response_header_4() {
-    const HEADER: &str = "RR0009";
+    const HEADER: &[u8] = b"RR0009";
 
-    let _ = ResponseHeader::from_str(HEADER).unwrap();
+    let _ = ResponseHeader::try_from(HEADER).unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_response_header_5() {
-    const HEADER: &str = "R10009";
+    const HEADER: &[u8] = b"R10009";
 
-    let _ = ResponseHeader::from_str(HEADER).unwrap();
+    let _ = ResponseHeader::try_from(HEADER).unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_response_header_6() {
-    const HEADER: &str = "2120009";
+    const HEADER: &[u8] = b"2120009";
 
-    let _ = ResponseHeader::from_str(HEADER).unwrap();
+    let _ = ResponseHeader::try_from(HEADER).unwrap();
 }
