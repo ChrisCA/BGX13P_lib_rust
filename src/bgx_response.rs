df3cf42d1f24870fbcdef8f5e03b6dc0fbc2e19b
Christian Albrecht <christian.albrecht@lorentz.de>
2022-01-14 18:18:23 +0100

    parse module response from bytes with nom

--- src/bgx_response.rs
+++ src/bgx_response.rs
@@ -1,3 +1,11 @@
+use anyhow::{anyhow, Context, Error};
+use log::{debug, trace};
+use nom::{
+    bytes::complete::{take, take_till},
+    character::complete::{char, crlf, digit1},
+    error::VerboseError,
+    sequence::delimited,
+};
 use strum::Display;
 use thiserror::Error;
 
@@ -37,6 +45,73 @@ impl From<u8> for ResponseCodes {
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum ModuleResponse {
-    DataWithHeader(ResponseHeader, String),
+    DataWithHeader(ResponseHeader, (Vec<u8>, String, Vec<u8>)),
     DataWithoutHeader(Vec<u8>),
+}
+
+impl TryFrom<&[u8]> for ModuleResponse {
+    type Error = Error;
+
+    /// takes input, returns optional content before, the actual content and the optional content after
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        /*
+        SAMPLE:
+        R000029\r\n
+        BGX13P.1.2.2738.2-1524-2738\r\n
+        */
+        debug!("BGX answered: {:?}", value);
+
+        // split everything off before the 'R'
+        let (after, before_header) = take_till(|c| c == b'R')(value)
+            .map_err(|e: nom::Err<VerboseError<_>>| anyhow!("{}", e))
+            .context("Didn't get any data when reading from BGX")?;
+
+        // early return if no 'R' is found
+        if after.is_empty() {
+            return Ok(ModuleResponse::DataWithoutHeader(before_header.to_vec()));
+        }
+
+        // get out the relevant numbers from the header
+        let (module_message, header) = delimited(char('R'), digit1, crlf)(after)
+            .map_err(|e: nom::Err<VerboseError<_>>| anyhow!("{}", e))?;
+
+        // parse header
+        let header = ResponseHeader::try_from(header)?;
+        trace!("Parsed header: {:?}", header);
+
+        // split of the part of the module answer which has been communicated via the header
+        let (after_message, module_message) = take(header.length)(module_message)
+            .map_err(|e: nom::Err<VerboseError<_>>| anyhow!("{}", e))?;
+
+        let module_message = std::str::from_utf8(module_message)?;
+
+        Ok(ModuleResponse::DataWithHeader(
+            header,
+            (
+                before_header.to_vec(),
+                module_message.to_string(),
+                after_message.to_vec(),
+            ),
+        ))
+    }
+}
+
+#[test]
+fn module_response_test_1() {
+    const input1: &[u8] = b"R000029\r\nBGX13P.1.2.2738.2-1524-2738\r\n";
+
+    assert_eq!(
+        ModuleResponse::DataWithHeader(
+            ResponseHeader {
+                response_code: ResponseCodes::Success,
+                length: 29
+            },
+            (
+                Vec::new(),
+                "BGX13P.1.2.2738.2-1524-2738\r\n".to_string(),
+                Vec::new()
+            )
+        ),
+        ModuleResponse::try_from(input1).unwrap()
+    )
 }
